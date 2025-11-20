e6ed6f70efced96778b400aeab7d2a07c3af9fb6
Andrea Cheok <andreacheok@gmail.com>
2025-11-19 21:34:16 -0800

    nov18

--- include/robot-config.h
+++ include/robot-config.h
@@ -6,8 +6,13 @@ extern motor FrontRight;
 extern motor FrontLeft;
 extern motor BackRight;
 extern motor BackLeft;
+extern motor Middle;
+extern motor Outtake1;
+extern motor Outtake2
 
 void vexcodeInit(void);
 
 extern motor_group Left;
 extern motor_group Right;
+extern motor_group Intake;
+extern motor_group Outtake;
