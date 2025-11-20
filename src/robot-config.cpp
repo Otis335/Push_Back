3e9aeb855d777d0e8d9eb543765e070632dd0a8d
Andrea Cheok <andreacheok@gmail.com>
2025-11-19 21:32:01 -0800

    nov18

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -7,14 +7,13 @@ using code = vision::code;
 brain Brain;
 
 controller Controller;
-motor FrontRight = motor(PORT1, ratio18_1, false); //not updated, port number change
-motor FrontLeft = motor(PORT2, ratio18_1, true);
-motor BackRight = motor(PORT3, ratio18_1, false);
-motor BackLeft = motor(PORT4, ratio18_1, true);
-motor Front = motor(PORT10, ratio6_1); //port undetermined, unfinished below
-motor Middle = motor(PORT11, ratio6_1); //port undetermined
-motor Outtake1 = motor(PORT12,ratio6_1); //port undetermined
-motor Outtake2 = motor(PORT13,ratio6_1); //port undertermined
+motor FrontRight = motor(PORT11, ratio18_1, false); 
+motor FrontLeft = motor(PORT14, ratio18_1, true);
+motor BackRight = motor(PORT13, ratio18_1, false);
+motor BackLeft = motor(PORT12, ratio18_1, true);
+motor Middle = motor(PORT8, ratio6_1, true); 
+motor Outtake1 = motor(PORT6,ratio6_1, false); 
+motor Outtake2 = motor(PORT7,ratio6_1, true); 
 
 void vexcodeInit(void) {}
 
